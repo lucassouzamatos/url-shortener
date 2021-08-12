952aa29b7e4c4fde1199ce10fdcba2ff929927bc
lucas.matos <lucas.ssouza.mmatos@gmail.com>
2021-08-12 18:30:34 -0300

    add lein structure

--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -1,7 +1,27 @@
 (ns url-shortener.core
+  (:require [org.httpkit.server :as server]
+    [compojure.core :refer :all]
+    [compojure.route :as route]
+    [ring.middleware.defaults :refer :all]
+    [clojure.pprint :as pp]
+    [clojure.string :as str]
+    [clojure.data.json :as json])
   (:gen-class))
 
+(defn hello-name [req]
+    {:status  200
+      :headers {"Content-Type" "text/html"}
+      :body    (->
+                (pp/pprint req)
+                (str "Hello " (:name (:params req))))})
+
+(defroutes app-routes
+  (GET "/hello" [] hello-name)
+  (route/not-found "Error, page not found!"))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "This is our main entry point"
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "4000"))]
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
