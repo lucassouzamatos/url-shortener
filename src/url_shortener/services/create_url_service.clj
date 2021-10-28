9b3b3ac6cf39e8919693378da6f5f23dade849a1
lucassouzamatos <lucas.ssouza.mmatos@gmail.com>
2021-10-27 23:30:08 -0300

    add url hash

--- src/url_shortener/services/create_url_service.clj
+++ src/url_shortener/services/create_url_service.clj
@@ -1,6 +1,11 @@
 (ns url-shortener.services.create_url_service
-  (:require [url-shortener.repositories.url_repository :as url_repository]))
+  (:require [url-shortener.repositories.url_repository :as url_repository])
+  (:import clojure.lang.Murmur3))
+
+(defn hash-url
+  [url] 
+  (format "%x" (Murmur3/hashUnencodedChars url)))
 
 (defn generate-url
   [url]
-  (url_repository/create-uri url url))
+  (url_repository/create-uri url (hash-url url)))
