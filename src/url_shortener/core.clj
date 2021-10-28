4144daa2743b3180aae71ae821dfd3e6dc9eca70
lucassouzamatos <lucas.ssouza.mmatos@gmail.com>
2021-10-27 21:28:06 -0300

    add example wrapping user

--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -9,9 +9,19 @@
             [url-shortener.infra.routes :as infra-routes])
   (:gen-class))
 
+;; this is an only test for wrapping fn
+;; can got user in handlers above:
+;; (defn handler [req]
+;;   (let [user (:user req)]
+;;     (println "user -> " user)
+(defn wrap-user 
+  [handler]
+  (fn ([request]
+        (handler (merge-with into request {:user 1})))))
+
 (defn -main
   "This is our main entry point"
   [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "4000"))]
-    (server/run-server (wrap-defaults #'infra-routes/app-routes site-defaults) {:port port})
+    (server/run-server (wrap-user (wrap-defaults #'infra-routes/app-routes site-defaults)) {:port port})
     (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
